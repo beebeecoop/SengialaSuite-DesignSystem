bc06b65023723a3c07e4a7aa8425434c0d683a69
beebeecoop <koperasibelatabelantaraberhad@gmail.com>
2026-07-27 09:12:57 +0800

    feat(sdc): expose public component API

---
+++ packages/sengiala_component_library/lib/sengiala_component_library.dart
@@ -0,0 +1,13 @@
+library sengiala_component_library;
+
+export 'src/components/sdc_avatar.dart';
+export 'src/components/sdc_badge.dart';
+export 'src/components/sdc_button.dart';
+export 'src/components/sdc_card.dart';
+export 'src/components/sdc_chip.dart';
+export 'src/components/sdc_icon_button.dart';
+export 'src/components/sdc_progress_indicator.dart';
+export 'src/components/sdc_surface.dart';
+export 'src/components/sdc_text_field.dart';
+export 'src/foundation/sdc_tokens.dart';
+export 'src/theme/sdc_theme.dart';
